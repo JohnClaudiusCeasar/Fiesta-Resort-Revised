b19165710d1895857314fe37be0c1d0c86f4ea2e
Bacarro <bacarro.johnclaude@gmail.com>
2026-03-08 19:15:43 +0800

    Starting Point - Where Everything Begins

---
+++ resources/pages/client/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div style="background: #f0f0f0; height: 100vh; display: flex; align-items: center; justify-content: center;">
+    <h1 style="color: #333; font-family: sans-serif;">
+      Success! Your Clean Inertia Stack is Live.
+    </h1>
+  </div>
+</template>
